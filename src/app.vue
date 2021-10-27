af328a47860677d6cec30b12679863d0edd10330
eeroom <eedroom@163.com>
2021-10-27 17:16:17 +0800

    组件示例

--- src/app.vue
+++ src/app.vue
@@ -1,31 +1,19 @@
 <script>
-import BllAccount from '../bll/BllAccount'
+import index from './views/index.vue'
 
-let bllAccount = new BllAccount();
+
 export default {
-  methods:{
-    clikcme(){
-      console.log("clikcme",this.$store.state);
-      bllAccount.setState({count:(this.data.count||0)+1})
-    }
-  },
-  computed: {
-    data(){
-      console.log("computed-data",this.$store);
-      return this.$store.state[bllAccount.namespace]||{}}
+  components:{
+    index
   }
-
 }
 </script>
 
 <template>
-  <p class="greeting">{{ data.count||0 }}</p>
-  <button v-on:click="clikcme">点我一下</button>
+  <index></index>
+   <index></index>
 </template>
 
 <style>
-.greeting {
-  color: red;
-  font-weight: bold;
-}
+
 </style>
--- src/views/index.vue
+++ src/views/index.vue
@@ -1,3 +1,31 @@
+<script>
+import BllAccount from '../../bll/BllAccount'
+
+let bllAccount = new BllAccount();
+export default {
+  methods:{
+    clikcme(){
+      console.log("clikcme",this.$store.state);
+      bllAccount.setState({count:(this.data.count||0)+1})
+    }
+  },
+  computed: {
+    data(){
+      console.log("computed-data",this.$store);
+      return this.$store.state[bllAccount.namespace]||{}}
+  }
+
+}
+</script>
+
 <template>
-  <h1>Index</h1>
+  <p class="greeting">{{ data.count||0 }}</p>
+  <button v-on:click="clikcme">点我一下</button>
 </template>
+
+<style>
+.greeting {
+  color: red;
+  font-weight: bold;
+}
+</style>
