c56117d2a0bc56a939fc18a38e8e46e5eb3f3d8c
eeroom <eedroom@163.com>
2021-10-27 17:27:27 +0800

    组件的props示例

--- src/app.vue
+++ src/app.vue
@@ -10,8 +10,8 @@ export default {
 </script>
 
 <template>
-  <index></index>
-   <index></index>
+  <index mybanji="三年二班"></index>
+   <index mybanji="幼儿媛"></index>
 </template>
 
 <style>
--- src/views/index.vue
+++ src/views/index.vue
@@ -3,6 +3,7 @@ import BllAccount from '../../bll/BllAccount'
 
 let bllAccount = new BllAccount();
 export default {
+  props:["mybanji"],//如果要自定义属性，必须这里声明，否则引用方传值不报错，但组件内取不到值
   methods:{
     clikcme(){
       console.log("clikcme",this.$store.state);
@@ -19,6 +20,7 @@ export default {
 </script>
 
 <template>
+  <p>班级：{{mybanji}}</p>
   <p class="greeting">{{ data.count||0 }}</p>
   <button v-on:click="clikcme">点我一下</button>
 </template>
