47caee37992ca016bb7c667f4d8f39a8079630ad
eeroom <eedroom@163.com>
2021-11-22 11:21:49 +0800

    组合式api

--- view/account/myinfo.vue
+++ view/account/myinfo.vue
@@ -1,38 +1,32 @@
 <script>
 import BllAccount from "../../bll/BllAccount";
-
+import { useStore } from "vuex";
+import { computed, ref, onMounted } from "vue";
 let bllAccount = new BllAccount();
 export default {
   props: ["mybanji"], //如果要自定义属性，必须这里声明，否则引用方传值不报错，但组件内取不到值
-  methods: {
-    clikcme() {
-      console.log("clikcme", this.$store.state);
-      bllAccount.setState({ count: (this.mystate.count || 0) + 1 });
-      this.myclicktimes=this.myclicktimes+1;
-    },
-    getNowDate() {
-      return new Date();
-    },
-  },
-  computed: {
-    mystate() {
-      console.log("mystate,‘第2个’执行", this.$store);
-      return this.$store.state[bllAccount.namespace] || {};
-    },
-    wangp() {
-      console.log("wangp,‘第2个’执行", this.$store);
-      return new Date()?{a:"1"}:{b:"2"};
-    },
-  },
-  mounted() {
-    console.log("mounted,这个方法在组件生命周期内只执行一次，并且'第3个'执行");
-    bllAccount.setState({ count: 112 });
-  },
-  data() {
-    console.log("data，这个方法在组件生命周期内只执行一次，并且'第1个'执行");
+   setup(props) {
+    let store = useStore();
+    let mystate = computed(() => store.state[bllAccount.namespace] || {});
+    let myclicktimes = ref(0);
+    let clikcme = () => {
+      bllAccount.setState({ count: (mystate.value.count || 0) + 1 });
+      myclicktimes.value += 1;
+    };
+    let wangp = computed(() => (new Date() ? { a: "1" } : { b: "2" }));
+    let nowDate = new Date();
+    onMounted(() => {
+      setTimeout(() => {
+        //bllAccount.setState({ count: 112 });
+      }, 3000);
+    });
     return {
-      nowDate: this.getNowDate(),
-      myclicktimes:0
+      mystate,
+      myclicktimes,
+      wangp,
+      nowDate,
+      clikcme,
+      bllAccount,
     };
   },
 };
--- view/index.vue
+++ view/index.vue
@@ -1,49 +1,51 @@
 <script>
 import BllAccount from "../bll/BllAccount";
-
+import { useStore } from "vuex";
+import { computed, ref, onMounted } from "vue";
 let bllAccount = new BllAccount();
 export default {
   props: ["mybanji"], //如果要自定义属性，必须这里声明，否则引用方传值不报错，但组件内取不到值
-  methods: {
-    clikcme() {
-      console.log("clikcme", this.$store.state);
-      bllAccount.setState({ count: (this.mystate.count || 0) + 1 });
-      this.myclicktimes=this.myclicktimes+1;
-    },
-    getNowDate() {
-      return new Date();
-    },
-  },
-  computed: {
-    mystate() {
-      console.log("mystate,‘第2个’执行", this.$store);
-      return this.$store.state[bllAccount.namespace] || {};
-    },
-    wangp() {
-      console.log("wangp,‘第2个’执行", this.$store);
-      return new Date()?{a:"1"}:{b:"2"};
-    },
-  },
-  mounted() {
-    console.log("mounted,这个方法在组件生命周期内只执行一次，并且'第3个'执行");
-    bllAccount.setState({ count: 112 });
-  },
-  data() {
-    console.log("data，这个方法在组件生命周期内只执行一次，并且'第1个'执行");
+  setup(props) {
+    let store = useStore();
+    let mystate = computed(() => store.state[bllAccount.namespace] || {});
+    let myclicktimes = ref(0);
+    let clikcme = () => {
+      bllAccount.setState({ count: (mystate.value.count || 0) + 1 });
+      myclicktimes.value += 1;
+    };
+    let wangp = computed(() => (new Date() ? { a: "1" } : { b: "2" }));
+    let nowDate = new Date();
+    onMounted(() => {
+      setTimeout(() => {
+        //bllAccount.setState({ count: 112 });
+      }, 3000);
+    });
     return {
-      nowDate: this.getNowDate(),
-      myclicktimes:0
+      mystate,
+      myclicktimes,
+      wangp,
+      nowDate,
+      clikcme,
+      bllAccount,
     };
   },
 };
 </script>
 
 <template>
-   <h3><router-link to="/">首页22</router-link></h3>
-   <h3><router-link to="/account/myinfo">我的信息</router-link></h3>
-  <p>班级：{{ mybanji }}{{ this.nowDate }}{{wangp}}</p>
+  <h3><router-link to="/">首页22</router-link></h3>
+  <h3><router-link to="/account/myinfo">我的信息</router-link></h3>
+  <p>班级：{{ mybanji }}{{ this.nowDate }}{{ wangp }}</p>
+  <input
+    type="text"
+    :value="mystate.count"
+    v-on:input="
+      (x) => bllAccount.setState({ count: Number.parseInt(x.target.value) })
+    "
+  />
+  <input type="text" v-model="mystate.count" />
   <p class="greeting">mystate:{{ mystate.count || 0 }}</p>
-  <p >data:{{ myclicktimes }}</p>
+  <p>data:{{ myclicktimes }}</p>
   <button v-on:click="clikcme">点我一下</button>
 </template>
 
