8789357eaa653c802964974fd06ec27523733b93
eeroom <eedroom@163.com>
2021-12-07 14:44:49 +0800

    --------------------

--- view/index.vue
+++ view/index.vue
@@ -1,22 +1,25 @@
 <script>
-import {  ref,  reactive } from "vue";
+import { ref, reactive } from "vue";
 import vinput from "../component/vinput.vue";
 import vform from "../component/vform.vue";
 export default {
   components: { vinput, vform },
   setup() {
     let vvf = ref(null);
-
- let formdata = reactive({ mani: {} });
-let submitform = () => {
+    let formdata = ref({ });
+    let submitform = () => {
       vvf.value.validata(function () {
-        console.log(arguments);
+        alert(JSON.stringify(formdata.value));
       });
+    };
+      let setformdata = () => {
+        formdata.value={seek:'we',seek22:'123456'}
     };
     return {
       submitform,
+      setformdata,
       formdata,
-      vvf
+      vvf,
     };
   },
 };
@@ -27,25 +30,31 @@ let submitform = () => {
   <h3><router-link to="/account/myinfo">我的信息</router-link></h3>
 
   <vform ref="vvf">
-     <div class="form-group">
-        <label for="exampleInputEmail1">Email address</label>
-        <vinput type="text" class="form-control" v-model="formdata.mani.seek" />
+    <div class="form-group">
+      <label for="exampleInputEmail1">Email address</label>
+      <vinput type="text" class="form-control" v-model="formdata.seek" />
     </div>
     <div class="form-group">
-        <label for="exampleInputPassword1">Password</label>
-        <vinput type="password" class="form-control" v-model="formdata.mani.seek22" />
+      <label for="exampleInputPassword1">Password</label>
+      <vinput
+        type="password"
+        class="form-control"
+        v-model="formdata.seek22"
+      />
     </div>
     <div class="form-group">
-        <label for="exampleInputFile">File input</label>
-        <input type="file" id="exampleInputFile" />
-        <p class="help-block">Example block-level help text here.</p>
+      <label for="exampleInputFile">File input</label>
+      <input type="file" id="exampleInputFile" />
+      <p class="help-block">Example block-level help text here.</p>
     </div>
     <div class="checkbox">
-        <label>
-            <input type="checkbox" /> Check me out
-        </label>
+      <label> <input type="checkbox" /> Check me out </label>
     </div>
-    <button type="submit" class="btn btn-primary" v-on:click="submitform">确定</button>
+    <div class="form-group"> <button  class="btn btn-primary" v-on:click="setformdata">设置一个新的formdata值</button></div>
+
+    <div class="form-group"><button  class="btn btn-primary" v-on:click="submitform">校验并提交</button></div>
+    
+   
   </vform>
 </template>
 
@@ -54,7 +63,7 @@ let submitform = () => {
   color: red;
   font-weight: bold;
 }
-.form-group{
+.form-group {
   position: relative;
 }
 </style>
