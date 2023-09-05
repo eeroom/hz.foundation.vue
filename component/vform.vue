7e4619159b9b0487559d389fa7bcfab951c1ce0a
eeroom <eedroom@163.com>
2023-09-05 12:07:48 +0800

    优化vform

--- component/vform.vue
+++ component/vform.vue
@@ -3,25 +3,21 @@
     <slot></slot>
   </form>
 </template>
-<script>
+<script setup>
 import { provide, ref } from "vue";
-export default {
-  setup(props, { emit, slots, attrs }) {
-    let lstValidat = [];
-    provide("lstValidat", lstValidat);
-    let validata = (callback) => {
-      let rts = [];
-      lstValidat.forEach((x) => {
-        if (!x.flag) return;
-        rts.push(x.handler());
-      });
-      let vrt = !rts.filter((x) => x).length > 0;
-      callback("校验完成", vrt);
-    };
 
-    return {
-      validata,
-    };
-  },
+let lstValidat = [];
+provide("lstValidat", lstValidat);
+let validata = (callback) => {
+  let rts = [];
+  lstValidat.forEach((x) => {
+    if (!x.flag) return;
+    rts.push(x.handler());
+  });
+  let vrt = !rts.filter((x) => x).length > 0;
+  callback("校验完成", vrt);
 };
+
+defineExpose({validata})
+
 </script>
