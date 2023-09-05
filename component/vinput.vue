9501127edd51f0122ae20d8a3da862d9c0953611
eeroom <eedroom@163.com>
2023-09-05 12:04:21 +0800

    优化vinput

--- component/vinput.vue
+++ component/vinput.vue
@@ -7,53 +7,44 @@
   />
   <div v-if="vfault" class="v-tips">{{ vmsg }}</div>
 </template>
-<script>
+<script setup>
 import { ref, inject, onMounted, onUnmounted } from "vue";
-export default {
-  props: ["modelValue", "validate", "class",'type'],
-  emits: ["update:modelValue"],
-  setup(props, { emit, slots, attrs }) {
-    let lstValidat = inject("lstValidat");
-    let vfault = ref(false);
-    let vmsg = ref("");
-    let inputHandler = (x) => {
-      emit("update:modelValue", x.target.value);
-      validataf(x.target.value);
-    };
-    let myclass = props.class;
-    let validataf = (x) => {
-      if (!x) {
-        vmsg.value = "不能为空";
-        vfault.value = true;
-      } else if (x.length < 3) {
-        vfault.value = true;
-        vmsg.value = "至少包含3个字符";
-      } else if (x.length > 6) {
-        vfault.value = true;
-        vmsg.value = "不能超过6个字符";
-      } else {
-        vfault.value = false;
-      }
-      return vfault.value;
-    };
-    let wp = {
-      flag: true,
-      handler: () => validataf(props.modelValue),
-    };
-    onMounted(() => {
-      lstValidat.push(wp);
-    });
-    onUnmounted(() => {
-      wp.flag = false;
-    });
-    return {
-      inputHandler,
-      vfault,
-      vmsg,
-      myclass,
-    };
-  },
+let props=defineProps( ["modelValue", "validate", "class",'type'])
+let emit=defineEmits( ["update:modelValue"])
+
+let lstValidat = inject("lstValidat");
+let vfault = ref(false);
+let vmsg = ref("");
+let inputHandler = (x) => {
+  emit("update:modelValue", x.target.value);
+  validataf(x.target.value);
 };
+let myclass = props.class;
+let validataf = (x) => {
+  if (!x) {
+    vmsg.value = "不能为空";
+    vfault.value = true;
+  } else if (x.length < 3) {
+    vfault.value = true;
+    vmsg.value = "至少包含3个字符";
+  } else if (x.length > 6) {
+    vfault.value = true;
+    vmsg.value = "不能超过6个字符";
+  } else {
+    vfault.value = false;
+  }
+  return vfault.value;
+};
+let wp = {
+  flag: true,
+  handler: () => validataf(props.modelValue),
+};
+onMounted(() => {
+  lstValidat.push(wp);
+});
+onUnmounted(() => {
+  wp.flag = false;
+});
 </script>
 <style>
 .form-control {
